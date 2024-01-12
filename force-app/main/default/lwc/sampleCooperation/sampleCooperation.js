ee3998c6e40e93a426922ba645659515c900d8db
devops 管理者 <mizuki.hana@imprich-create.site>
2024-01-12 09:14:13 +0000

    WI-000008で追加したAexクラスのメソッドやLWCを追加修正

--- force-app/main/default/lwc/sampleCooperation/sampleCooperation.js
+++ force-app/main/default/lwc/sampleCooperation/sampleCooperation.js
@@ -2,15 +2,15 @@ import { LightningElement, wire } from 'lwc';
 import LightningAlert from 'lightning/alert';
 import getNow from '@salesforce/apex/Sample.getNow';
 export default class SampleCooperation extends LightningElement {
-  message;
+  displayMessage;
   
-  @wire(getNow)
+  @wire(getNow, { message: '⭐️ページ表示時刻: ' })
   wiredGetNow({ error, data }) {
     if (data) {
-      this.message = data;
+      this.displayMessage = data;
       this.error = undefined;
     } else if (error) {
-      this.message = undefined;
+      this.displayMessage = undefined;
       LightningAlert.open({
         message: 'ApexクラスメソッドgetNowコール時に予期せぬエラーが発生しました。' + error.body.message,
         theme: 'error',
